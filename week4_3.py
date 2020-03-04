c53f5d424eaed7a660e7eccaa497ec63b4fa6b7f
Susanne Lundkvist <susannelundkvist@Susannes-MacBook-Pro.local>
2020-03-04 10:51:36 +0100

    Copenhagen string

---
+++ week4_3.py
@@ -0,0 +1,5 @@
+coaches = 5
+destination = "copenhagen"
+fast_train = False 
+
+print('This train has ' + str(coaches) + ' coaches and is going to ' + str.upper(destination) + ' but it would be ' + str(fast_train) + ' to say it is a fast train.')
