d938e3bd107fcccb6b0ba03eaad762290f0cf24f
Susanne Lundkvist <susannelundkvist@Susannes-MacBook-Pro.local>
2020-03-04 11:50:52 +0100

    using dictionary

---
+++ week4_4.py
@@ -0,0 +1,10 @@
+dic = {"name":"Brynhilde", "favourite_colour":"green", "coolest_word":"ice", "best_laser_colour":"purple"}
+
+
+
+print('The name of a main character in Valkyrian is ' + dic["name"] + '.')
+print('My favourite colour is ' + (dic.get("favourite_colour")) + '.')
+print('The coolest word in the world is ' + dic["coolest_word"] + '.')
+print('The most enticing laser colour is ' + (dic.get("best_laser_colour")) + '.')
+
+
